ce644c64d1af047cbd1e0dc4559521bcb5e27c17
AdamHtooLwin <adamhtoolwin@gmail.com>
2021-03-21 20:57:22 +0700

    Initial commit with skeletal project

---
+++ src/main/java/com/coco/cocotrace/CocotraceApplication.java
@@ -0,0 +1,13 @@
+package com.coco.cocotrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocotraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocotraceApplication.class, args);
+	}
+
+}

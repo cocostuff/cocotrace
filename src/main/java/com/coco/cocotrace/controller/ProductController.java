b1f98b490a44ceeb5d1820b4959fee2c49dcb13a
titouanlm <titouan.le-mao@etu.unice.fr>
2021-04-07 12:17:35 +0700

    Add product without ingredients

--- src/main/java/com/coco/cocotrace/controller/ProductController.java
+++ src/main/java/com/coco/cocotrace/controller/ProductController.java
@@ -1,18 +1,42 @@
 package com.coco.cocotrace.controller;
 
 import com.coco.cocotrace.models.Product;
+import com.coco.cocotrace.models.User;
+import com.coco.cocotrace.service.ProductServiceImpl;
+import com.coco.cocotrace.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.security.Principal;
+
 @Controller
 public class ProductController {
+
+    @Autowired
+    private ProductServiceImpl productService;
+
+    @Autowired
+    private UserServiceImpl userService;
 
     @RequestMapping(path = "/addProduct", method= RequestMethod.GET)
     public String create(Model model) {
         model.addAttribute("product", new Product());
         return "product.jsp";
+    }
+
+    @RequestMapping(value = { "/addProduct" }, method = RequestMethod.POST)
+    public String shoppingCartUpdateQty(Model model, Principal principal, @ModelAttribute("product") Product product) {
+
+        User u = userService.findByUsername(principal.getName());
+        product.setUser(u);
+
+        productService.save(product);
+
+        return "redirect:/productsList";
     }
 
 }
--- src/main/webapp/product.jsp
+++ src/main/webapp/product.jsp
@@ -1,6 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,7 +12,32 @@
 <body>
 <h1>Add new Product</h1>
 
+<form:form method="POST" modelAttribute="product" action="/addProduct">
 
+        <div class="product-preview-container">
+            <ul>
+                <li>Name:
+                    <form:input path="name"/>
+                </li>
+                <li>Manufacturing Origin:
+                    <form:input path="manufacturingOrigin"/>
+                </li>
+                <li>Manufacturing Date:
+                    <form:input path="manufacturingDateString" type="date"/>
+                </li>
+                <li>Consumed before:
+                    <form:input path="consumedBeforeString" type="date"/>
+                </li>
+                <li>Weight:
+                    <form:input path="weight" />
+                </li>
+
+            </ul>
+        </div>
+
+    <input class="button-update-sc" type="submit" value="Add Product" />
+
+</form:form>
 
 </body>
 </html>

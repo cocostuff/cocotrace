288463771db89ee62a50554d087a97f78c7b3d78
cedriclemercier <info@cedricdesigns.com.au>
2021-04-23 09:26:13 +0700

    Added Linebot Controller, config files and dependencies

---
+++ src/main/java/com/coco/cocotrace/controller/LineBotController.java
@@ -0,0 +1,33 @@
+package com.coco.cocotrace.controller;
+
+import com.coco.cocotrace.CocotraceApplication;
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/callback")
+@LineMessageHandler
+public class LineBotController {
+    private final Logger log = LoggerFactory.getLogger(CocotraceApplication.class);
+
+    @EventMapping
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        log.info("event: " + event);
+        final String originalMessageText = event.getMessage().getText();
+        return new TextMessage(originalMessageText);
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+    }
+}

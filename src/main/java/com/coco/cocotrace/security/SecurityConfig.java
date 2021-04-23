ebcaab1ed953f2945f4be274e6162bd7ff404fe1
cedriclemercier <info@cedricdesigns.com.au>
2021-04-23 17:04:46 +0700

    Allowed /callback path to accessible in SecurityConfig

--- src/main/java/com/coco/cocotrace/security/SecurityConfig.java
+++ src/main/java/com/coco/cocotrace/security/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable()		
 		.authorizeRequests()
-		.antMatchers("/h2-console/**", "/login", "/register").permitAll()
+		.antMatchers("/h2-console/**", "/login", "/register", "/callback").permitAll()
 		.antMatchers("/admin/**").hasRole("ADMIN")
 		.antMatchers("/**").hasAnyRole("ADMIN", "USER")
         .and()
